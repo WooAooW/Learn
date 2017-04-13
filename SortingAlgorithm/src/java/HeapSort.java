0a5a5519e683ca9a5d33713f39146559d8fcac18
Kiss <346886697@qq.com>
2017-04-13 15:15:00 +0800

    新增所有排序算法

---
+++ SortingAlgorithm/src/java/HeapSort.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 堆排序.
+ * 核心思想: .
+ * 
+ * @see https://heleeos.com/blog/sorting-algorithm-theory.html#5堆排序
+ * @author liyu
+ */
+public class HeapSort {
+    
+    public static void main(String[] args) {
+        List<Integer> data = getData(20);
+        data.forEach(num -> System.out.printf("%s,", num));
+        System.out.println();
+        
+                
+        data.forEach(num -> System.out.printf("%s,", num));
+    }
+
+    /**
+     * 生成一组数据
+     */
+    public static List<Integer> getData(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        for(int i = 0; i < n; i++)
+            list.add(i, i);
+        Collections.shuffle(list);//打乱顺序
+        return list;
+    }
+}
--- SortingAlgorithm/src/java/ShellSort.java
+++ SortingAlgorithm/src/java/ShellSort.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 /**
- * 插入排序.
- * 核心思想: 选择小的元素插入到前面.
+ * 希尔排序.
+ * 核心思想: 分组进行插入排序.
  * 
- * @see https://heleeos.com/blog/sorting-algorithm-theory.html#3插入排序
+ * @see https://heleeos.com/blog/sorting-algorithm-theory.html#4希尔排序
  * @author liyu
  */
-public class InsertionSort {
+public class ShellSort {
 
     public static void main(String[] args) {
         List<Integer> data = getData(20);
@@ -16,23 +17,23 @@ public class InsertionSort {
         System.out.println();
         
         int size = data.size();
-        for(int i = 1; i < size; i++) {
-            int number = data.get(i);
+        for(int gap = size >> 1; gap > 0; gap = gap >> 1) {
             
-            //把当前的元素插入到已排序元素的对应位置
-            for(int index = i - 1; index >= 0; index--) {
-                if(number < data.get(index)) {
-                    data.set(index + 1, data.get(index));
-                    if(index == 0) {
-                        data.set(index, number);
+            //对每一个元素进行插入排序
+            for(int i = gap; i < size; i++) {
+                int temp = data.get(i);
+                int j;
+                for(j = i - gap; j >= 0; j -= gap) {
+                    if(temp < data.get(j)) {
+                        data.set(j + gap, data.get(j));
+                    } else {
+                        break;
                     }
-                } else {
-                    data.set(index + 1, number);
-                    break;
                 }
+                data.set(j + gap, temp);
             }
             
-            System.out.printf("第 %s 次:", i);
+            System.out.printf("步长为:%s,", gap);
             data.forEach(num -> System.out.printf("%s ", num));
             System.out.println();
         }
