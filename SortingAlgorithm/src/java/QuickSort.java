2402d470af9642e0e36e916cd5e1dd46242bd492
Kiss <346886697@qq.com>
2017-04-14 17:41:27 +0800

    新增快速排序的递归实现

--- SortingAlgorithm/src/java/QuickSort.java
+++ SortingAlgorithm/src/java/QuickSort.java
@@ -4,19 +4,21 @@ import java.util.List;
 
 /**
  * 快速排序.
- * 核心思想: .
+ * 核心思想: 分而治之,小的放左边,大的放右边.
  * 
  * @see https://heleeos.com/blog/sorting-algorithm-theory.html#7快速排序
  * @author liyu
  */
 public class QuickSort {
 
+    //递归的次数
+    public static int recursiveCount = 1;
+    
     public static void main(String[] args) {
         List<Integer> data = getData(20);
         data.forEach(num -> System.out.printf("%s,", num));
         System.out.println();
-        
-                
+        quickSortRecursive(data, 0, data.size() - 1);                
         data.forEach(num -> System.out.printf("%s,", num));
     }
 
@@ -29,5 +31,42 @@ public class QuickSort {
             list.add(i, i);
         Collections.shuffle(list);//打乱顺序
         return list;
+    }
+    
+    /**
+     * 交换两个元素
+     **/
+    public static void swap(List<Integer> data, int index1, int index2) {
+        int temp = data.get(index1);
+        data.set(index1, data.get(index2));
+        data.set(index2, temp);
+    }
+    
+    /**
+     * 递归实现快速排序.
+     * 
+     * @param start 开始下标
+     * @param end   结束下标
+     */
+    public static void quickSortRecursive(List<Integer> data, int start, int end) {
+        if(start >= end) 
+            return;
+        int number = data.get(end);
+        int left = start;
+        int right = end;
+        while(left < right) {
+            while(data.get(left) < number && left < right) {
+                left++;
+            }
+            while(data.get(right) > number && left < right) {
+                right--;
+            }
+            swap(data, left, right);
+        }
+        System.out.printf("第 %s 次:", recursiveCount++);
+        data.forEach(num -> System.out.printf("%s ", num));
+        System.out.println();
+        quickSortRecursive(data, start, left - 1);
+        quickSortRecursive(data, left + 1, end);
     }
 }
