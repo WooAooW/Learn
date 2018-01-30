1be414b15f5acb5d09d2d807b85ae43aae50dfb5
liyu <liyu26@meituan.com>
2018-01-30 19:19:22 +0800

    新增练习题

--- Asm/5.asm
+++ Asm/5.asm
@@ -5,6 +5,21 @@ assume cs:code
 code segment
 
 	mov ax, 0ffffh
+	mov ds, ax
+
+	mov bx, 0
+	mov cx, 0bh
+	mov dx, 0
+
+	s:
+		mov ax, [bx]
+		mov ah, 0
+		add dx, ax
+		inc bx       ;相当于 add bx, 1
+		loop s
+
+	mov ax, 4c00h
+	int 21h
 
 code ends
 
---
+++ Asm/9.asm
@@ -0,0 +1,25 @@
+;把 mov ax, 4c00h 之前的指令复制到 0:200 处
+
+assume cs:code
+
+code segment
+
+	mov ax, cs
+	mov ds, ax
+
+	mov ax, 0020h
+	mov es, ax
+
+	mov bx, 0
+	mov cx, 17h         ;需要先填入一个值后，查看 mov ax, 4c00h 的位置
+	s:
+		mov al, [bx]
+		mov es:[bx], al
+		inc bx
+		loop s
+	mov ax, 4c00h
+	int 21h
+
+code ends
+
+end
